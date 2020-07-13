d1d201945cb004056e08e0f2454117651db89a1d
Altela Pramardhika <altela.pramardhika@gmail.com>
2020-07-13 11:44:47 +0800

    Initial Commit

---
+++ planetarium/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  planetarium
+//
+//  Created by Altela Pramardhika on 13/07/20.
+//  Copyright © 2020 projectflakes. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
