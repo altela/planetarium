161ca7e2751b64a24d678fbb0f400b65637f8962
Altela Pramardhika <altela.pramardhika@gmail.com>
2020-11-04 16:48:05 +0800

    Applying and replacing all function of button using switch case

--- planetarium/navigation/ARMenuController.swift
+++ planetarium/navigation/ARMenuController.swift
@@ -11,70 +11,44 @@ import UIKit
 class ARMenuController: UIViewController {
 
     @IBOutlet weak var showAR: UIButton!
-        @IBOutlet weak var toMerkurius: UIButton!
-            @IBOutlet weak var toVenus: UIButton!
-                @IBOutlet weak var toBumi: UIButton!
-                    @IBOutlet weak var toMars: UIButton!
-                @IBOutlet weak var toJupiter: UIButton!
-            @IBOutlet weak var toSaturnus: UIButton!
-        @IBOutlet weak var toUranus: UIButton!
-    @IBOutlet weak var toNeptunus: UIButton!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
     
-
     @IBAction func showAR(_ sender: UIButton) {
-        print(sender.currentTitle!)
         
         switch sender.currentTitle {
         case "sun":
             performSegue(withIdentifier: "toMatahariSegue", sender: self)
+            print("AR view of \(sender.currentTitle!) successfully executed")
+        case "mercury":
+            performSegue(withIdentifier: "toMerkuriusSegue", sender: self)
+            print("AR view of \(sender.currentTitle!) successfully executed")
+        case "venus":
+            performSegue(withIdentifier: "toVenusSegue", sender: self)
+            print("AR view of \(sender.currentTitle!) successfully executed")
+        case "earth":
+            performSegue(withIdentifier: "toBumiSegue", sender: self)
+            print("AR view of \(sender.currentTitle!) successfully executed")
+        case "mars":
+            performSegue(withIdentifier: "toMarsSegue", sender: self)
+            print("AR view of \(sender.currentTitle!) successfully executed")
+        case "jupiter":
+            performSegue(withIdentifier: "toJupiterSegue", sender: self)
+            print("AR view of \(sender.currentTitle!) successfully executed")
+        case "saturn":
+            performSegue(withIdentifier: "toSaturnusSegue", sender: self)
+            print("AR view of \(sender.currentTitle!) successfully executed")
+        case "uranus":
+            performSegue(withIdentifier: "toUranusSegue", sender: self)
+            print("AR view of \(sender.currentTitle!) successfully executed")
+        case "neptune":
+            performSegue(withIdentifier: "toNeptunusSegue", sender: self)
+            print("AR view of \(sender.currentTitle!) successfully executed")
         default:
             print("Not Available")
         }
     }
-    
-    @IBAction func toMerkuriusAction(_ sender: Any) {
-        performSegue(withIdentifier: "toMerkuriusSegue", sender: self)
-            print("Tombol Merkurius Ditekan")
-    }
-    
-    @IBAction func toVenusAction(_ sender: Any) {
-        performSegue(withIdentifier: "toVenusSegue", sender: self)
-            print("Tombol Venus Ditekan")
-    }
-    
-    @IBAction func toBumiAction(_ sender: Any) {
-        performSegue(withIdentifier: "toBumiSegue", sender: self)
-            print("Tombol Bumi Ditekan")
-    }
-    
-    @IBAction func toMarsAction(_ sender: Any) {
-        performSegue(withIdentifier: "toMarsSegue", sender: self)
-            print("Tombol Mars Ditekan")
-    }
-    
-    @IBAction func toJupiterAction(_ sender: Any) {
-        performSegue(withIdentifier: "toJupiterSegue", sender: self)
-            print("Tombol Jupiter Ditekan")
-    }
-    
-    @IBAction func toSaturnusButton(_ sender: Any) {
-        performSegue(withIdentifier: "toSaturnusSegue", sender: self)
-            print("Tombol Saturnus Ditekan")
-    }
-    
-    @IBAction func toUranusButton(_ sender: Any) {
-        performSegue(withIdentifier: "toUranusSegue", sender: self)
-            print("Tombol Uranus Ditekan")
-    }
-    
-    @IBAction func toNeptunusButton(_ sender: Any) {
-        performSegue(withIdentifier: "toNeptunusSegue", sender: self)
-            print("Tombol Neptunus Ditekan")
-    }
-}
 
+}
