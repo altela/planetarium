69fc0310d0d71509a6c1dfd9a47e9d52d48081c5
Altela Eleviansyah Pramardhika <altela_pramardhika@yahoo.com>
2021-04-06 19:21:35 +0800

    Fix Merge Conflict

---
+++ planetarium/Model/PlanetNodes.swift
@@ -0,0 +1,22 @@
+//
+//  Nodes.swift
+//  planetarium
+//
+//  Created by Altela Pramardhika on 17/11/20.
+//  Copyright © 2020 projectflakes. All rights reserved.
+//  Write for testing commits
+
+import UIKit
+import SceneKit
+import ARKit
+
+struct PlanetNodes {
+    let nodes = SCNNode()
+    let position = SCNVector3(-0.3, 0.1, -0.7)
+}
+
+struct DescriptionNodes{
+    let nodes = SCNNode()
+    let position = SCNVector3(0.1, -0.1, -0.9)
+    let scale = SCNVector3(0.008, 0.008, 0.01)
+}
